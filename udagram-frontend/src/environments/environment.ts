0b4fb19219cc2eb516e7c141763c5bb1e28f0b84
Beza <getbez@gmail.com>
2023-12-03 23:20:54 +0100

    add configs for frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -1,11 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+const API_HOST = process.env.API_HOST
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: `${API_HOST}:8080/api/v0`
 };
 
 /*
